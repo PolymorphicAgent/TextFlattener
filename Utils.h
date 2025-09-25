f5c485cb32b30bd39874ab3b6e3daee50a4269f8
PolymorphicAgent <polymorphicagent07@gmail.com>
2025-09-25 12:07:36 -0400

    Upload skeleton files

---
+++ Utils.h
@@ -0,0 +1,10 @@
+/// Utility functions
+
+#include <string>
+
+// Converts string to binary
+// in cpp 14+, you can represent binary values as unsigned integers:
+// Ex: unsigned int test = 0b10101010
+unsigned int strToBin(std::string s){
+    
+}
