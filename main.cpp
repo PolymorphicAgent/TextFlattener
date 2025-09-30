640f3fa066a658688046bb45d51dd955b1c40f4f
PolymorphicAgent <polymorphicagent07@gmail.com>
2025-09-30 09:27:24 -0400

    Add references

--- main.cpp
+++ main.cpp
@@ -13,3 +13,11 @@ int main(int argc, char *argv[]){
     return 0;
       
 }
+
+/*
+    References:
+
+    - https://en.cppreference.com/w/cpp/container/unordered_map/insert.html
+    - https://stackoverflow.com/questions/19197799/what-is-the-quickest-way-of-inserting-updating-stdunordered-map-elements-witho
+
+*/
