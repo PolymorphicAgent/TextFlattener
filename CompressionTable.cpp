d43588d4cd63a26dfcca8c32c8a80674c6f32422
PolymorphicAgent <polymorphicagent07@gmail.com>
2025-09-30 08:49:33 -0400

    Provide skeleton functions

--- CompressionTable.cpp
+++ CompressionTable.cpp
@@ -1,1 +1,48 @@
 #include "CompressionTable.h"
+
+CompressionTable::CompressionTable(File* file, Mode mode) : 
+    m_file(file), m_mode(mode), m_map_binStr(nullptr), 
+    m_map_strBin(nullptr), m_header(nullptr)
+{
+    if(mode == Compress)
+        m_map_strBin = new std::unordered_map<std::string, std::vector<bool>>();
+    else
+        m_map_binStr = new std::unordered_map<std::vector<bool>, std::string>();
+}
+
+void CompressionTable::setHeader(std::vector<bool>& header){
+    if(m_mode == Compress)
+        throw std::runtime_error("Invalid attempt to set header in compress mode!");
+    else {
+        delete m_header;
+        m_header = &header;
+    }
+    return;
+}
+
+bool CompressionTable::genHeader(){//TODO: implement
+    // Assume mappings have been generated
+    return true;
+}
+
+std::string CompressionTable::mapBinToStr(const std::vector<bool>& bin) const {
+    if(m_mode == Compress)
+        throw std::runtime_error("Invalid attempt to map binary to string in compress mode!");
+
+    auto it = m_map_binStr->find(bin);
+    if(it != m_map_binStr->end())
+        return it->second;
+    else //TODO: This failure case is for easier debugging. In the future, we should map unknown characters to '#'
+        throw std::runtime_error("Binary sequence not found in compression table!");
+}
+
+std::vector<bool> CompressionTable::mapStrToBin(const std::string& str) const {
+    if(m_mode == Decompress)
+        throw std::runtime_error("Invalid attempt to map string to binary in decompress mode!");
+
+    auto it = m_map_strBin->find(str);
+    if(it != m_map_strBin->end())
+        return it->second;
+    else //TODO: This failure case is for easier debugging. In the future, we should map unknown characters to a special binary sequence
+        throw std::runtime_error("String not found in compression table!");
+}
--- CompressionTable.h
+++ CompressionTable.h
@@ -1,9 +1,33 @@
 #pragma once
 
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+#include "File.h"
 
 /// @brief This class represents a dynamic compression table (aka dictionary) that will provide conversion
 /// @brief functions to and from shortened binary representations.
 class CompressionTable {
-    public:
+public:
+    enum Mode {Compress, Decompress};
 
+    explicit CompressionTable(File* file, Mode mode);
+
+    File* getFile() const { return m_file; }
+    Mode getMode() const { return m_mode; }
+
+    std::vector<bool>* getHeader() const { return m_header; }
+    void setHeader(std::vector<bool>&);
+    bool genHeader();
+
+    std::string mapBinToStr(const std::vector<bool>& bin) const;
+    std::vector<bool> mapStrToBin(const std::string& str) const;
+
+private:
+    File* m_file; // Always Read-Only
+    Mode m_mode; // Always Read-Only
+    std::vector<bool> *m_header; // Read-Only in Compress Mode, Read-Write in Decompress Mode
+    std::unordered_map<std::vector<bool>, std::string> *m_map_binStr; // Initialized in Decompress Mode ONLY
+    std::unordered_map<std::string, std::vector<bool>> *m_map_strBin; // Initialized in Compress Mode ONLY
 };
