0aa18ece03702a148be05ec228faea164c0b6190
PolymorphicAgent <polymorphicagent07@gmail.com>
2025-10-01 12:16:56 -0400

    Add frequency calculation test

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char *argv[]){
     
     Utils::testFileIO();
 
+    Utils::testCharFreqs();
+
     return 0;
       
 }
@@ -17,7 +19,9 @@ int main(int argc, char *argv[]){
 /*
     References:
 
-    - https://en.cppreference.com/w/cpp/container/unordered_map/insert.html
+    - https://en.cppreference.com/w/cpp/container/unordered_map.html
     - https://stackoverflow.com/questions/19197799/what-is-the-quickest-way-of-inserting-updating-stdunordered-map-elements-witho
+    - https://stackoverflow.com/questions/5056645/sorting-stdmap-using-value
+    - https://stackoverflow.com/questions/42933943/how-to-use-lambda-for-stdfind-if
 
 */
